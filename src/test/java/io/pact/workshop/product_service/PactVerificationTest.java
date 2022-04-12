e3da3d4d2b9da20b6dac0f391d26f500333b43ba
sonzqn <sonzqn@gmail.com>
2022-04-12 16:42:47 +0700

    update

--- src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
+++ src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
@@ -25,7 +25,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Provider("provider-x")
+@Provider("provider-java")
 @PactBroker
 public class PactVerificationTest {
     @LocalServerPort
