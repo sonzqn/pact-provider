8cabb2881ea48a2e1fdad9ba6c38506023e7157d
son.nguyenduc <sonnd4@tcbs.com.vn>
2022-06-24 10:09:39 +0700

    remove code

--- src/main/java/io/pact/workshop/product_service/products/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package io.pact.workshop.product_service.products;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
